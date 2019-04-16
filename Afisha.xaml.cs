ecfc22f1534358d25347262ac7119eaaec2b989a
Stasencia <44728575+Stasencia@users.noreply.github.com>
2019-04-16 14:39:41 +0300

    Performance_list

--- Afisha.xaml.cs
+++ Afisha.xaml.cs
@@ -18,24 +18,21 @@ namespace Theater
     /// <summary>
     /// Логика взаимодействия для Afisha.xaml
     /// </summary>
+    public enum Months
+    {
+        Январь = 1, Февраль, Март, Апрель, Май, Июнь, Июль, Август, Сентябрь, Октябрь, Ноябрь, Декабрь
+    }
     public partial class Afisha
     {
-        public enum Months
-        {
-            Январь = 1, Февраль, Март, Апрель, Май, Июнь, Июль, Август, Сентябрь, Октябрь, Ноябрь, Декабрь
-        }
+        private int Month_id { get; set; }
         public Afisha()
         {
             InitializeComponent();
-            performancelist_panel.Children.Add(new Performance_list());
-            performancelist_panel.Children.Add(new Performance_list());
-            performancelist_panel.Children.Add(new Performance_list());
             Month_buttons();
         }
 
         private void Month_buttons()
         {
-            int i = 0;
             Months m;
             DataContext db = new DataContext(DB_connection.connectionString);
             var query2 = db.GetTable<TAfisha_dates>()
@@ -53,7 +50,10 @@ namespace Theater
                             })
                             .OrderBy(k => k.key1).ThenBy(k => k.key2)
                             .Take(4);
-
+            if(query2.Any())
+            {
+                Month_id = query2.First().key2;
+            }
             foreach (var q in query2)
             {
                 Button b = new Button();
@@ -61,10 +61,48 @@ namespace Theater
                 b.Content = m + Environment.NewLine + q.key1;
                 b.Style = this.FindResource("MonthStyle") as Style;
                 b.Tag = q.key2;
-                //b.Click += new System.EventHandler(this.button1_Click);
+                b.Click += new RoutedEventHandler(this.MonthBtn_Click);
                 month_panel.Children.Add(b);
-                i++;
             }
+        }
+
+        private void MonthBtn_Click(object sender, EventArgs e)
+        {
+            Refresh(Convert.ToInt32(((Control)sender).Tag));
+        }
+
+        public void Refresh(int month)
+        {
+            performancelist_panel.Children.Clear();
+            Month_id = month;
+            DataContext db = new DataContext(DB_connection.connectionString);
+            var query2 = db.GetTable<TAfisha_dates>()
+                        .Where(k => k.Date.Month == Month_id && k.Date >= DateTime.Now && !k.Cancelled)
+                        .Select(k => k.Id_performance)
+                        .Distinct().ToArray();
+            var query1 = db.GetTable<TAfisha>()
+                        .Where(k => query2.Contains(k.Id))
+                        .Select(k => new { k.Image, k.Name, k.Duration, k.Age_restriction, k.Description, k.Id });
+            foreach (var q in query1)
+            {
+                Performance_list p = new Performance_list();
+                string s = DB_connection.current_directory + "images_afisha\\" + q.Image;
+                p.PerfImage.Source = new BitmapImage(new Uri(s, UriKind.RelativeOrAbsolute));
+                p.PerfName.Content = q.Name;
+                p.Duration.Content = q.Duration;
+                p.AgeRestriction.Content = q.Age_restriction;
+                p.Description.Text = q.Description;
+                p.LearnMoreBtn.Tag = q.Id;
+                p.LearnMoreBtn.Click += new RoutedEventHandler(this.LearnMoreBtn_Click);
+                performancelist_panel.Children.Add(p);
+            }
+        }
+
+        private void LearnMoreBtn_Click(object sender, EventArgs e)
+        {
+            //Performance f = new Performance(Convert.ToInt32(((Control)sender).Tag), (Afisha)this.Parent.Parent, Month_id);
+            //f.Show();
+            //this.Parent.Parent.Hide();
         }
     }
 }
---
+++ TAfisha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq.Mapping;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theater
+{
+    [Table(Name = "Afisha")]
+    class TAfisha
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+        [Column]
+        public string Name { get; set; }
+        [Column]
+        public string Image { get; set; }
+        [Column]
+        public string Duration { get; set; }
+        [Column]
+        public string Age_restriction { get; set; }
+        [Column]
+        public string Description { get; set; }
+        [Column]
+        public string Big_image { get; set; }
+        [Column]
+        public string Small_info { get; set; }
+        [Column]
+        public string Small_name { get; set; }
+        [Column]
+        public string Small_image { get; set; }
+        [Column]
+        public double Price { get; set; }
+        [Column]
+        public bool Is_relevant { get; set; }
+    }
+}
